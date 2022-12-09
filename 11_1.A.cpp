26b7e83e50911184619f9e85060e16a94a9be2ba
RidGamerQ <118125622+RidGamerQ@users.noreply.github.com>
2022-12-09 18:15:17 +0200

    Add files via upload

---
+++ 11_1.A.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    char nazva;
+    cout << "\n Enter the first letter of the country from the list: Ukraine, America, Poland, Turkey, Sweden, Germany, Bulgaria: ";
+    cin >> nazva;
+    switch (nazva)
+    {
+    case 'U': cout << "\n Ukraine: population - 44 million, number of cities - 272"; break;
+    case 'A': cout << "\n America: population - 332 million, number of cities - 30000"; break;
+    case 'P': cout << "\n Poland: population - 38 million, number of cities - 964"; break;
+    case 'T': cout << "\n Turkey: population - 85 million, number of cities - 200"; break;
+    case 'S': cout << "\n Sweden: population - 10.5 million, number of cities - 96"; break;
+    case 'G': cout << "\n Germany: population - 83 million, number of cities - 2054"; break;
+    case 'B': cout << "\n Bulgaria: population - 7 million, number of cities - 96"; break;
+    default: cout << "\n You entered an incorrect letter, please enter a capital letter U, A, P, T, S, G, or B!"; break;
+    }
+
+}
---
+++ 11_1.B.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    char nazva;
+    cout << "\n Enter the first letter of the country from the list: Ukraine, America, Poland, Turkey, Sweden: ";
+    cin >> nazva;
+    if (nazva == 'U')
+    {
+        cout << "\n Ukraine: population - 44 million, number of cities - 272";
+    }
+    else if (nazva == 'A')
+    {
+        cout << "\n America: population - 332 million, number of cities - 30000";
+    }
+    else if (nazva == 'P')
+    {
+        cout << "\n Poland: population - 38 million, number of cities - 964";
+    }
+    else if (nazva == 'T')
+    {
+        cout << "\n Turkey: population - 85 million, number of cities - 200";
+    }
+    else if (nazva == 'S')
+    {
+        cout << "\n Sweden: population - 10.5 million, number of cities - 96";
+    }
+    else if (nazva == 'G')
+    {
+        cout << "\n Germany: population - 83 million, number of cities - 2054";
+    }
+    else if (nazva == 'B')
+    {
+        cout << "\n Bulgaria: population - 7 million, number of cities - 96";
+    }
+
+    else
+    {
+        cout << "\n You entered an incorrect letter, please enter a capital letter U, A, P, T, S, G, or B!";
+    }
+
+
+
+}
